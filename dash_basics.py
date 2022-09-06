b0eb1ed238239bdd7869c48f37140dbdc0583498
Ben Smith <ben.f.smith07@gmail.com>
2022-09-06 17:00:01 -0500

    ...

--- dash_basics.py
+++ dash_basics.py
@@ -4,16 +4,42 @@ from dash import html
 
 app = dash.Dash()
 
-app.layout = html.Div(children=[html.H1('Hello Dash'),
-                                html.Div('Dash: Web Dashboards with Python'),
-                                dcc.Graph(id='example',
-                                          figure={'data':[{'x':[1,2,3], 'y':[4,1,2], 'type':'bar', 'name':'SF'},
+colors = {
+          'background': '#111111', # black
+          'text': '#7FDBFF'         # teal
+         }
+
+app.layout = html.Div(children=[
+                                html.H1(
+                                        children='Example Dashboard',
+                                        style={
+                                               'textAlign': 'center',
+                                               'color': colors['text']
+                                              }
+                                         ),
+                                html.H2('Subtitle Here',
+                                         style={
+                                                'textAlign': 'center',
+                                                'color': colors['text']
+                                               }
+                                        ),
+                                dcc.Graph(
+                                          id='example',
+                                          figure={
+                                                  'data':[
+                                                          {'x':[1,2,3], 'y':[4,1,2], 'type':'bar', 'name':'SF'},
                                                           {'x':[1,2,3], 'y':[2,4,5], 'type':'bar', 'name':'NYC'}
                                                          ],
-                                                  'layout':{'title': 'BAR PLOTS'}
+                                                  'layout':{
+                                                            'plot_bgcolor': colors['background'],
+                                                            'paper_bgcolor': colors['background'],
+                                                            'font':{'color':colors['text']},
+                                                            'title': 'Example Bar Plots'
+                                                           }
                                                  }
                                          )
-                               ]
+                               ],
+                        style={'backgroundColor':colors['background']}
                      )
 
 if __name__ == '__main__':
