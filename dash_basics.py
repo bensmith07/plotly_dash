34d1e30a833379a09b4a76213a94f08978b38192
Ben Smith <ben.f.smith07@gmail.com>
2022-09-06 11:28:49 -0500

    ...

---
+++ dash_basics.py
@@ -0,0 +1,20 @@
+import dash
+from dash import dcc
+from dash import html
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[html.H1('Hello Dash'),
+                                html.Div('Dash: Web Dashboards with Python'),
+                                dcc.Graph(id='example',
+                                          figure={'data':[{'x':[1,2,3], 'y':[4,1,2], 'type':'bar', 'name':'SF'},
+                                                          {'x':[1,2,3], 'y':[2,4,5], 'type':'bar', 'name':'NYC'}
+                                                         ],
+                                                  'layout':{'title': 'BAR PLOTS'}
+                                                 }
+                                         )
+                               ]
+                     )
+
+if __name__ == '__main__':
+    app.run_server()
